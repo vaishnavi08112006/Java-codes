9c032663b66051b2e1ef38a7af969484724ae5c8
vaishnavi08112006 <vaishnavi11082006@gmail.com>
2026-07-21 22:09:29 +0530

    Create Uppercase and lowercase.java

---
+++ Uppercase and lowercase.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        String name = "vaishnavi";
+
+        System.out.println(name.toUpperCase());
+        System.out.println(name.toLowerCase());
+    }
+}
