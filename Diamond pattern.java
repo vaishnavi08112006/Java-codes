c339fc4f474ff8d2bed57e34e87bde4c368a53b8
vaishnavi08112006 <vaishnavi11082006@gmail.com>
2026-07-23 15:12:12 +0530

    Create Diamond pattern.java

---
+++ Diamond pattern.java
@@ -0,0 +1,24 @@
+public class DiamondPattern{
+    public static void main(String[]args){
+        for(int i = 1; i<=3; i++){
+            for(int j=3;j > i; j--){
+                System.out.print(" ");
+            }
+            for(int k = 1; k<= (2 * i -1);k++){
+                System.out.print("*");
+            }
+            System.out.println();
+            
+        }
+            for(int i=2;i >=1;i--){
+                for(int j = 3 ;j > i;j--){
+                    System.out.print(" ");
+                }
+                for(int k = 1;k<=(2 * i-1);k++){
+                    System.out.print("*");
+                }
+                System.out.println();
+                }
+            }
+        }      
+     
