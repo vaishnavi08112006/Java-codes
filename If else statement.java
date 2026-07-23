68c3440d346296a7d6bec3014141bf0fd82f3e34
vaishnavi08112006 <vaishnavi11082006@gmail.com>
2026-07-23 17:32:15 +0530

    Create If else statement.java

---
+++ If else statement.java
@@ -0,0 +1,14 @@
+ public class passfail{
+     public static void main (String[]args){
+         int Marks = 45;
+         if(Marks>=50){
+             System.out.println("pass");
+         }
+         else
+         {
+             System.out.println("fail");
+         }
+     }
+ }
+        
+      
