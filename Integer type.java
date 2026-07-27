6537129cd0b01cb1d774b7e91db5c79950aa5eea
vaishnavi08112006 <vaishnavi11082006@gmail.com>
2026-07-27 16:44:14 +0530

    Create Integer type.java

---
+++ Integer type.java
@@ -0,0 +1,15 @@
+public class main{
+    public static void main(String []args){
+        {
+            byte a = 99;
+            short b = 30000;
+            int c = 480000;
+            System.out.println("Mark"+a);
+            System.out.println("Monthly salary"+b);
+            System.out.println("Annual income"+c);
+        }
+    }
+}
+
+
+
