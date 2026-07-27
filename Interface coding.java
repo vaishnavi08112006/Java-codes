d0d97be2fde73385db9d81ec999b9e52c4f173ce
vaishnavi08112006 <vaishnavi11082006@gmail.com>
2026-07-27 15:19:11 +0530

    Create Interface coding.java

---
+++ Interface coding.java
@@ -0,0 +1,16 @@
+interface Animal{
+    void sound();
+}
+class Dog implements Animal{
+    public void sound(){
+        System.out.println("Dog barks");
+    }
+}
+public class Main{
+    public static void main(String[]args){
+        Dog d=new Dog();
+        d.sound();
+    }
+}
+    
+
