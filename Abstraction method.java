24c2100b4e3ef90c9cacd226fafff77d05e61f5e
vaishnavi08112006 <vaishnavi11082006@gmail.com>
2026-07-27 14:57:50 +0530

    Create Abstraction method.java

---
+++ Abstraction method.java
@@ -0,0 +1,19 @@
+abstract class Animal{
+    abstract void sound();
+    void sleep(){
+        System.out.println("Animal is sleeping");
+    }
+}
+class Dog extends Animal{
+    void sound(){
+        System.out.println("Dog barks");
+    }
+}
+public class Main{
+    public static void main(String[]args){
+        Dog d=new Dog();
+        d.sound();
+        d.sleep();
+    }
+}
+    
