eb525c247e025d244328d40e72bf207efddcf337
vaishnavi08112006 <vaishnavi11082006@gmail.com>
2026-07-29 16:42:13 +0530

    Create Even number.java

---
+++ Even number.java
@@ -0,0 +1,14 @@
+public class main{
+    public static void main(String[]args){
+    {
+     int number=8;
+     if(number%2==0){
+         System.out.println("Even number");
+     }
+     else
+     {
+         System.out.println("odd number");
+     }
+    }  
+    }
+}
