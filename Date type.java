fe589859ee1b9bd258f126cc2f0be4de0624c001
vaishnavi08112006 <vaishnavi11082006@gmail.com>
2026-07-29 16:30:53 +0530

    Create Date type.java

---
+++ Date type.java
@@ -0,0 +1,18 @@
+public class main{
+    public static void main(String[]args){
+    {
+        String str1="vaishnavi";
+        String str2="B.E-EEE";
+        char c='A';
+        String address="5/101 Magudanchavadi,kunnipalayam,salem";
+        int pincode=637103;
+        double cgpa=8.7d;
+        System.out.println("Name"+str1);
+        System.out.println("Class"+str2);
+        System.out.println("section"+c);
+        System.out.println("Address"+ address);
+        System.out.println("Pincode"+ pincode);
+        System.out.println("Cgpa"+ cgpa);
+    }    
+ }
+}
