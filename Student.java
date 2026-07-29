df65ae2740e2e8bb7e4ce2db80a094048a87712a
vaishnavi08112006 <vaishnavi11082006@gmail.com>
2026-07-29 16:56:07 +0530

    Create Student.java

---
+++ Student.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int mark = 82;
+
+        if (mark >= 90) {
+            System.out.println("GRADE A");
+        } else if (mark >= 75) {
+            System.out.println("GRADE B");
+        } else if (mark >= 50) {
+            System.out.println("GRADE C");
+        } else {
+            System.out.println("Fail");
+        }
+    }
+}
