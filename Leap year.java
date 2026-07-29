488bc35fdb2e7f6d287b2716bf9ee4da986721bb
vaishnavi08112006 <vaishnavi11082006@gmail.com>
2026-07-29 17:59:16 +0530

    Create Leap year.java

---
+++ Leap year.java
@@ -0,0 +1,14 @@
+public class main{
+    public static void main(String[]args) {
+    int i = 2005;
+    while(i<=2026){
+        if(i%4==0){
+        System.out.println("LEAP YEAR");
+        i++;
+    } 
+    else{
+        System.out.println(i);
+        i++;}
+    }
+  }
+}
